7589305ba16771dda3dc3d7178c33a29e1c88e13
Sanne Bregman <sanne@bregman.email>
2023-11-30 18:43:58 +0100

    Switch to Starlight and add Github Action to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    compressHTML: true,
+    trailingSlash: 'always',
+    integrations: [
+        starlight({
+            title: 'The Network - A Pathfinder 2e Adventure',
+            social: {
+                github: 'https://github.com/xudonax/the-network',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', link: '/guides/example/' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+        }),
+    ],
+});
