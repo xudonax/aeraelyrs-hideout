2d7c24d7fc69e0fea66087ac88525fa365dc0e2a
Sanne Bregman <sanne@bregman.email>
2023-12-01 09:28:21 +0100

    Should've thought about this...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/the-network',
+    base: '/aeraelyrs-hideout/',
     site: 'https://www.xudonax.eu',
 
     compressHTML: true,
